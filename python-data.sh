9d2a295afd88380acd475bda429bcee7317f12c2
Vinay Gollena <96512285+vinay18-2@users.noreply.github.com>
2024-11-05 16:17:40 +0530

    Update python-data.sh

--- python-data.sh
+++ python-data.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-sudo yum -y update
-sudo yum -y install python3-pip
-git clone https://github.com/vinay18-2/Agri.git
-sudo mv Agri/ /home/ec2-user/
-cd /home/ec2-user/Agri/
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
+# Update the package list and install Python3 and pip
+sudo yum update -y
+sudo yum install -y python3 git
+
+# Install pip for Python 3
+sudo yum install -y python3-pip
+
+# Clone the application from your GitHub repository
+git clone https://github.com/vinay18-2/Agri.git /home/ec2-user/Agri
+
+# Navigate to the application directory
+cd /home/ec2-user/Agri
+
+# Install application dependencies from requirements.txt
+sudo pip3 install -r requirements.txt
+
+# Run the Python application (update the app.py path if necessary)
+nohup python3 app.py > app.log 2>&1 &
